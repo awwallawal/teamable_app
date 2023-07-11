a6bae9e608436089c82d5b764840c8631d284827
Awwal <awwallawal@gmail.com>
2023-07-11 22:05:22 +0100

    Used windows.onload, added a image uplod process  and img.src to manipulate the DOM

---
+++ app.js
@@ -0,0 +1,61 @@
+let imageUpload = document.getElementById("image-upload");
+let profilePicture = document.getElementById("profile-picture");
+
+let imageOutput = document.getElementById("image-output");
+
+let editContainer = document.getElementById("edit-container");
+
+let updateContainer = document.getElementById("update-container");
+
+let editBtn = document.getElementById("edit-btn");
+
+let updateBtn = document.getElementById("update-btn");
+
+let form = document.getElementById("form");
+
+let dummyPicture =  "./images/dummy-profile-pic.jpg";
+
+let userName = document.getElementById("user-name");
+
+let userEmail = document.getElementById("user-email");
+
+let userInterest = document.getElementById("user-interest");
+
+let profileName = document.getElementById("profile-name");
+
+
+let profileEmail = document.getElementById("profile-email");
+
+let profileInterest = document.getElementById("profile-interest");
+
+
+
+window.onload = (function(){
+ profilePicture.src = dummyPicture
+ imageOutput.src = dummyPicture
+ userName.value = profileName.textContent;
+ userEmail.value = profileEmail.textContent;
+ userInterest.value = profileInterest.textContent;
+})
+
+
+editBtn.addEventListener("click", function(){
+ editContainer.style.display = "flex";
+ updateContainer.style.display = "none";
+});
+
+// updateBtn.addEventListener("click", function(){
+//  editContainer.style.display = "none";
+//  updateContainer.style.display = "flex"
+// });
+
+imageUpload.addEventListener('change', function(e){
+ imageOutput.src = URL.createObjectURL(e.target.files[0]);
+ profilePicture.src = URL.createObjectURL(e.target.files[0]);
+});
+
+form.addEventListener('submit', function(e){
+ e.preventDefault();
+ editContainer.style.display = "none";
+ updateContainer.style.display = "flex"
+})
